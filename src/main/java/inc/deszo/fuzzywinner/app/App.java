4e4048c130779fb9edabdd26cba69525a76aed39
dessy888 <deszo.inc@gmail.com>
2017-06-07 10:44:03 +0100

    First commit

---
+++ src/main/java/inc/deszo/fuzzywinner/app/App.java
@@ -0,0 +1,13 @@
+package inc.deszo.fuzzywinner.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

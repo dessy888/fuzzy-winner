832527536a7eee8f1a087f05390fbda3d0d5fb50
dessy888 <deszo.inc@gmail.com>
2017-06-13 06:35:21 +0100

    improving code quality

---
+++ mongodb/queries/fundhistoryprices - GroupBySedolIsinFtSymbol.js
@@ -0,0 +1,10 @@
+db.fundhistoryprices.aggregate([
+    { $group: { 
+        _id: {"sedol":"$sedol", "isin":"$isin", "ftSymbol":"$ftSymbol"},
+        count: { $sum: 1 }
+    }},
+    { $sort : {count:-1} }
+ ], 
+    {allowDiskUse : true}
+)
+ 
